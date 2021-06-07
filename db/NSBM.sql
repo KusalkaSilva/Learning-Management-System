a9d7b97d8c181a7ae9a987d653ee4d9294dd321f
Kusalka Silva <kusalkasilva4@gmail.com>
2021-06-07 18:03:14 +0530

    Add files via upload

---
+++ db/NSBM.sql
@@ -0,0 +1,518 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 30, 2017 at 01:40 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `capstone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_log`
+--
+
+CREATE TABLE `activity_log` (
+  `activity_log_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `action` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `class_id` int(11) NOT NULL,
+  `class_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class_subject_overview`
+--
+
+CREATE TABLE `class_subject_overview` (
+  `class_subject_overview_id` int(11) NOT NULL,
+  `teacher_class_id` int(11) NOT NULL,
+  `content` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `department_id` int(11) NOT NULL,
+  `department_name` varchar(100) NOT NULL,
+  `dean` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL,
+  `event_title` varchar(100) NOT NULL,
+  `teacher_class_id` int(11) NOT NULL,
+  `date_start` varchar(100) NOT NULL,
+  `date_end` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `file_id` int(11) NOT NULL,
+  `floc` varchar(500) NOT NULL,
+  `fdatein` varchar(200) NOT NULL,
+  `fdesc` varchar(100) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `fname` varchar(100) NOT NULL,
+  `uploaded_by` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `notification_id` int(11) NOT NULL,
+  `teacher_class_id` int(11) NOT NULL,
+  `notification` varchar(100) NOT NULL,
+  `date_of_notification` varchar(50) NOT NULL,
+  `link` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification_read`
+--
+
+CREATE TABLE `notification_read` (
+  `notification_read_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `student_read` varchar(50) NOT NULL,
+  `notification_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification_read_teacher`
+--
+
+CREATE TABLE `notification_read_teacher` (
+  `notification_read_teacher_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `student_read` varchar(100) NOT NULL,
+  `notification_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school_year`
+--
+
+CREATE TABLE `school_year` (
+  `school_year_id` int(11) NOT NULL,
+  `school_year` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_id` int(11) NOT NULL,
+  `subject_code` varchar(100) NOT NULL,
+  `subject_title` varchar(100) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `unit` int(11) NOT NULL,
+  `Pre_req` varchar(100) NOT NULL,
+  `semester` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `teacher_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `about` varchar(500) NOT NULL,
+  `teacher_status` varchar(20) NOT NULL,
+  `teacher_stat` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_class`
+--
+
+CREATE TABLE `teacher_class` (
+  `teacher_class_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `thumbnails` varchar(100) NOT NULL,
+  `school_year` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_class_announcements`
+--
+
+CREATE TABLE `teacher_class_announcements` (
+  `teacher_class_announcements_id` int(11) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `teacher_id` varchar(100) NOT NULL,
+  `teacher_class_id` int(11) NOT NULL,
+  `date` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_class_student`
+--
+
+CREATE TABLE `teacher_class_student` (
+  `teacher_class_student_id` int(11) NOT NULL,
+  `teacher_class_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher_notification`
+--
+
+CREATE TABLE `teacher_notification` (
+  `teacher_notification_id` int(11) NOT NULL,
+  `teacher_class_id` int(11) NOT NULL,
+  `notification` varchar(100) NOT NULL,
+  `date_of_notification` varchar(100) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `assignment_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `firstname`, `lastname`) VALUES
+(15, 'admin', 'admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_log`
+--
+
+CREATE TABLE `user_log` (
+  `user_log_id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `login_date` varchar(30) NOT NULL,
+  `logout_date` varchar(30) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity_log`
+--
+ALTER TABLE `activity_log`
+  ADD PRIMARY KEY (`activity_log_id`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`class_id`);
+
+--
+-- Indexes for table `class_subject_overview`
+--
+ALTER TABLE `class_subject_overview`
+  ADD PRIMARY KEY (`class_subject_overview_id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`department_id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`file_id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notification_id`);
+
+--
+-- Indexes for table `notification_read`
+--
+ALTER TABLE `notification_read`
+  ADD PRIMARY KEY (`notification_read_id`);
+
+--
+-- Indexes for table `notification_read_teacher`
+--
+ALTER TABLE `notification_read_teacher`
+  ADD PRIMARY KEY (`notification_read_teacher_id`);
+
+--
+-- Indexes for table `school_year`
+--
+ALTER TABLE `school_year`
+  ADD PRIMARY KEY (`school_year_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_id`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`teacher_id`);
+
+--
+-- Indexes for table `teacher_class`
+--
+ALTER TABLE `teacher_class`
+  ADD PRIMARY KEY (`teacher_class_id`);
+
+--
+-- Indexes for table `teacher_class_announcements`
+--
+ALTER TABLE `teacher_class_announcements`
+  ADD PRIMARY KEY (`teacher_class_announcements_id`);
+
+--
+-- Indexes for table `teacher_class_student`
+--
+ALTER TABLE `teacher_class_student`
+  ADD PRIMARY KEY (`teacher_class_student_id`);
+
+--
+-- Indexes for table `teacher_notification`
+--
+ALTER TABLE `teacher_notification`
+  ADD PRIMARY KEY (`teacher_notification_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_log`
+--
+ALTER TABLE `user_log`
+  ADD PRIMARY KEY (`user_log_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity_log`
+--
+ALTER TABLE `activity_log`
+  MODIFY `activity_log_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `class_subject_overview`
+--
+ALTER TABLE `class_subject_overview`
+  MODIFY `class_subject_overview_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `department_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `notification_read`
+--
+ALTER TABLE `notification_read`
+  MODIFY `notification_read_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `notification_read_teacher`
+--
+ALTER TABLE `notification_read_teacher`
+  MODIFY `notification_read_teacher_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `school_year`
+--
+ALTER TABLE `school_year`
+  MODIFY `school_year_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teacher_class`
+--
+ALTER TABLE `teacher_class`
+  MODIFY `teacher_class_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teacher_class_announcements`
+--
+ALTER TABLE `teacher_class_announcements`
+  MODIFY `teacher_class_announcements_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teacher_class_student`
+--
+ALTER TABLE `teacher_class_student`
+  MODIFY `teacher_class_student_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `teacher_notification`
+--
+ALTER TABLE `teacher_notification`
+  MODIFY `teacher_notification_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT for table `user_log`
+--
+ALTER TABLE `user_log`
+  MODIFY `user_log_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
